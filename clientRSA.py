e40cc18a73728559fb5b7a898b1c8f1f33ac945e
lucasbeasley <labeasle@uncg.edu>
2017-04-07 09:04:51 -0400

    Server/client with RSA and transmitted string

---
+++ clientRSA.py
@@ -0,0 +1,30 @@
+#Name: Lucas Beasley
+#Purpose: Client file for project 2 with RSA implemented
+
+import socket
+import Crypto
+from Crypto.PublicKey import RSA
+
+#create a socket for the client
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect to server
+client_socket.connect(("localhost", 1234))
+
+#generate client keys
+ckey = RSA.generate(2048)
+publick = ckey.publickey().exportKey('DER')
+privatek = ckey.exportKey('DER')
+
+#send public key to server
+client_socket.send(publick)
+
+#receive server public key
+serverkey = client_socket.recv(2048)
+pubskey = RSA.importKey(serverkey)
+
+#send message to server
+sentstr = "Network Security"
+estr = pubskey.encrypt(sentstr, 'x')[0]
+client_socket.send(estr)
+
---
+++ serverRSA.py
@@ -0,0 +1,50 @@
+#Name: Lucas Beasley
+#Purpose: Server file for project 2 with RSA implemented
+
+import socket
+import Crypto
+from Crypto.PublicKey import RSA
+
+#create a socket for the server
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#setup server socket on machine
+try:
+    server_socket.bind(("localhost", 1234))
+
+except socket.error as error:
+    print error
+
+
+#wait for clients
+server_socket.listen(1234)
+
+#generate server keys
+skey = RSA.generate(2048)
+publick = skey.publickey().exportKey('DER')
+privatek = skey.exportKey('DER')
+privk = RSA.importKey(privatek)
+
+
+
+
+while 1:
+    try:
+        #accept clients
+        client, address = server_socket.accept()
+
+        #receive client public key
+        clientkey = client.recv(2048)
+        pubckey = RSA.importKey(clientkey)
+
+        #send public key to client
+        client.send(publick)
+
+        #receive and decrypt message from client
+        recstr = client.recv(2048)
+        dstr = privk.decrypt(recstr)
+        print dstr
+
+    except Exception as error:
+        print error
+
